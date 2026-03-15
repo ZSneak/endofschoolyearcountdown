770b0ef4240a8d80ae82dd2681f542619c4948b5
ZSneak <zeke.whitesides@gmail.com>
2026-03-14 23:00:14 -0400

    reworked the canvasapi function

--- resc/simplev2.canvasapi.js
+++ resc/simplev2.canvasapi.js
@@ -1,9 +1,14 @@
 async function getFromCanvas(apikey, canvasURL) {
     try {
-    response = await puter.net.fetch(`${canvasURL}/api/v1/courses?access_token=${apikey}?per_page=200`);
+    response = await puter.net.fetch(`${canvasURL}/api/v1/courses?per_page=200`, {
+        headers: {
+            'Authorization': `Bearer ${apikey}`,
+            'Content-Type': 'application/json'
+        }
+    });
     if (!response.ok) {throw new Error(`Response Status ${response.status}`); }
 
     result = await response.json();
     console.log(result);
-    } catch (error) { console.error(`Error fetching data. Error: ${error}`);}
+    } catch (error) { console.error(`Error fetching data. ${error}`);}
 }
