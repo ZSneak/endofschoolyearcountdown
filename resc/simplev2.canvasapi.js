faa98206ff791694497b8505a6d7d8cf5391ed2d
ZSneak <zeke.whitesides@gmail.com>
2026-03-14 22:36:06 -0400

    increased the responses per page

--- resc/simplev2.canvasapi.js
+++ resc/simplev2.canvasapi.js
@@ -1,6 +1,6 @@
 async function getFromCanvas(apikey, canvasURL) {
     try {
-    response = await puter.net.fetch(`${canvasURL}/api/v1/courses?access_token=${apikey}`);
+    response = await puter.net.fetch(`${canvasURL}/api/v1/courses?access_token=${apikey}?per_page=200`);
     if (!response.ok) {throw new Error(`Response Status ${response.status}`); }
 
     result = await response.json();
